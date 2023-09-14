2407e69bd94d869fdb987f7d49d49d15d9598c60
HardikParmar07 <hardik.parmar@nividata.com>
2023-09-14 15:18:05 +0530

    Revert "Django Task"
    This reverts commit 2faae0d6e180af9d75dd8ec33a2f00259d189157.

--- Django_practice/tasksite/taskpage/models.py
+++ Django_practice/tasksite/taskpage/models.py
@@ -1,24 +1,3 @@
 from django.db import models
 
-
-class User(models.Model):
-    user_name = models.CharField(max_length=200)
-    email = models.EmailField(max_length=254)
-    phone_number = models.CharField(max_length=10)
-    country = models.CharField(max_length=200)
-    message = models.CharField(max_length=200)
-    date = models.DateTimeField("Created on")
-
-    def __str__(self):
-        return self.email
-
-class Task(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
-    date = models.DateTimeField("Created on")
-    status = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.title
-
+# Create your models here.
