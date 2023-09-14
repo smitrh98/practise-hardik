f414da48701d0611e3256b98a9232e2438ef937d
HardikParmar07 <hardik.parmar@nividata.com>
2023-09-14 15:33:33 +0530

    Django Task final commit

--- Django_practice/tasksite/taskpage/models.py
+++ Django_practice/tasksite/taskpage/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    user_name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=254)
+    phone_number = models.CharField(max_length=10)
+    country = models.CharField(max_length=200)
+    message = models.CharField(max_length=200)
+    date = models.DateTimeField("Created on")
+
+    def __str__(self):
+        return self.email
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    date = models.DateTimeField("Created on")
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
--- Django_practice/tasksite/taskpage/urls.py
+++ Django_practice/tasksite/taskpage/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
 
+# app_name = "taskpage"
 urlpatterns = [
     path("", views.index, name="index"),
+    path('saveuser/', views.saveUser, name="saveuser"),
+    path('users-listing/', views.users, name='users-listing'),
+    path('<int:x>/task-listing/', views.tasks, name='task-listing'),
+    path('<int:user_id>/saveTask/', views.saveTask, name='saveTask'),
+    path('<int:task_id>/deleteTask/', views.deleteTask, name='deleteTask'),
+    path('<int:task_id>/updateTask/', views.updateTask, name='updateTask'),
+
 ]
