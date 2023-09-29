4e0d244396e53e1ad965582dc9f30077661662ce
HardikParmar07 <hardik.parmar@nividata.com>
2023-09-29 16:37:35 +0530

    Classbase views and User auth

---
+++ Django_practice/tasksite/superuser/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+# Create your views here.
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.success(request, ('Thers was an error in logging, Try again....'))
+            return redirect('login')
+
+    else:
+        return render(request, 'superuser/login.html', {})
--- Django_practice/tasksite/tasksite/urls.py
+++ Django_practice/tasksite/tasksite/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("taskpage.urls")),
+    path('',include('django.contrib.auth.urls')),
+    path('', include('superuser.urls')),
 ]
